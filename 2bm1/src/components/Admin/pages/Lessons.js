90b031c8dca8ac81c9d0ab5605ef00b71b45d932
Binqie <shamir.beishebaev@icloud.com>
2022-12-20 15:51:08 +0600

    admin/lessons form+methods work

--- 2bm1/src/components/Admin/pages/Lessons.js
+++ 2bm1/src/components/Admin/pages/Lessons.js
@@ -1,34 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import {TableControl} from 'react-bootstrap-table-control'
 
 function Lessons() {
+  const [lessonId, setId] = useState(0),
+        [lessonCode, setCode] = useState(''),
+        [lessonName, setName] = useState(''),
+        onInputId = ({target: {value}}) => {
+          setId(value);
+        },
+        onInputCode = ({target: {value}}) => {
+          setCode(value);
+        },
+        onInputName = ({target: {value}}) => {
+          setName(value);
+        }
+
+
+
+  const onSubmit = (type) => {
+    const data = {
+      id: lessonId,
+      code: lessonCode,
+      name: lessonName,
+    }
+    console.log(data, type);
+  }
+
   return (
     <>
       <div className="lessons-list">
         <TableControl
           header={[
             { key: "id", name: "Id" },
-            { key: "name", name: "Code" },
-            { key: "description", name: "Name" },
+            { key: "code", name: "Code" },
+            { key: "name", name: "Name" },
           ]}
           itens={[
-            { id: 1, name: "Name 1", description: "Description 1" },
-            { id: 2, name: "Name 2", description: "Description 2" },
-            { id: 3, name: "Name 3", description: "Description 3" },
-            { id: 4, name: "Name 4", description: "Description 4" },
-            { id: 5, name: "Name 5", description: "Description 5" },
-            { id: 1, name: "Name 1", description: "Description 1" },
-            { id: 2, name: "Name 2", description: "Description 2" },
-            { id: 3, name: "Name 3", description: "Description 3" },
-            { id: 4, name: "Name 4", description: "Description 4" },
-            { id: 5, name: "Name 5", description: "Description 5" },
-            { id: 1, name: "Name 1", description: "Description 1" },
-            { id: 2, name: "Name 2", description: "Description 2" },
-            { id: 3, name: "Name 3", description: "Description 3" },
-            { id: 4, name: "Name 4", description: "Description 4" },
-            { id: 5, name: "Name 5", description: "Description 5" },
+            { id: 1, code: "Name 1", name: "Description 1" },
+            { id: 2, code: "Name 2", name: "Description 2" },
+            { id: 3, code: "Name 3", name: "Description 3" },
+            { id: 4, code: "Name 4", name: "Description 4" },
+            { id: 5, code: "Name 5", name: "Description 5" },
           ]}
         />
       </div>
@@ -36,26 +50,50 @@ function Lessons() {
       <Form id="lessons-form">
         <Form.Group className="mb-3" controlId="formBasicId">
           <Form.Label>Lesson ID</Form.Label>
-          <Form.Control type="text" placeholder="Lesson ID" />
+          <Form.Control type="number" placeholder="Lesson ID" controlId="id"  onChange={onInputId}/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicCode">
           <Form.Label>Lesson code</Form.Label>
-          <Form.Control type="text" placeholder="Lesson code" />
+          <Form.Control type="text" placeholder="Lesson code" controlId="code" onChange={onInputCode}/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Lesson name</Form.Label>
-          <Form.Control type="text" placeholder="Lesson name" />
+          <Form.Control type="text" placeholder="Lesson name" controlId="name" onChange={onInputName}/>
         </Form.Group>
 
-        <Button className="form-btn add-btn" variant="primary" type="submit">
+        <div className="mb-3">
+          <Form.Check
+          className="radio-btn"
+            inline
+            label="Add lesson"
+            name="group1"
+            type='radio'
+          />
+          <Form.Check
+          className="radio-btn"
+            inline
+            label="Update lesson"
+            name="group1"
+            type='radio'
+          />
+          <Form.Check
+          className="radio-btn"
+            inline
+            label="Delete lesson"
+            name="group1"
+            type='radio'
+          />
+        </div>
+
+        <Button className="form-btn" variant="primary" type="submit" onClick={(event) => {event.preventDefault(); onSubmit('POST')}}>
           Add lesson
         </Button>
-        <Button className="form-btn update-btn" variant="primary" type="submit">
+        <Button className="form-btn" variant="primary" type="submit" onClick={(event) => {event.preventDefault(); onSubmit('UPDATE')}}>
           Update lesson
         </Button>
-        <Button className="form-btn delete-btn" variant="primary" type="submit">
+        <Button className="form-btn" variant="primary" type="submit" onClick={(event) => {event.preventDefault(); onSubmit('DELETE')}}>
           Delete lesson
         </Button>
       </Form>
